4f0effd78093cb3b404cefe5c4e3cfe876b8e126
Em <amberem@gmail.com>
2021-08-31 16:33:23 -0400

    Set up user, habit and daily record models.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
+from core.models import DailyRecord, Habit, User
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Habit)
+admin.site.register(DailyRecord)
+admin.site.register(User)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models import fields
+
 
 class User(AbstractUser):
     def __repr__(self):
         return f"<User username={self.username}>"
+
     def __str__(self):
         return self.username
+
+
+class Habit(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    goal = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="habits")
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class DailyRecord(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    habit = models.ForeignKey(
+        Habit, on_delete=models.CASCADE, related_name="daily_records")
+    constraint = models.UniqueConstraint(
+        fields=['habit', 'date'], name='unique_record')
+
+    def __repr__(self):
+        return f"<Daily Record date={self.date}>"
+
+    def __str__(self):
+        return f"{self.date}"
