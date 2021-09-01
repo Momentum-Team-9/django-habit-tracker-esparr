f57e19445042aa755e16ea495aa6a9501ae13b47
Em <amberem@gmail.com>
2021-09-01 12:59:14 -0400

    Editing foreignkey for DailyRecord model

--- core/models.py
+++ core/models.py
@@ -16,7 +16,7 @@ class Habit(models.Model):
     goal = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="habits", null=True, blank=True)
+        User, on_delete=models.CASCADE, related_name="habits", null=True)
 
     def __str__(self):
         return f"{self.title}"
@@ -25,7 +25,7 @@ class Habit(models.Model):
 class DailyRecord(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     habit = models.ForeignKey(
-        Habit, on_delete=models.CASCADE, related_name="daily_records", null=True, blank=True)
+        Habit, on_delete=models.CASCADE, related_name="daily_records", null=True)
 
     class Meta:
         constraints = [models.UniqueConstraint(
